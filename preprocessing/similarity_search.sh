a2f712eb0e6468ee80ae669bea16a91132c7fbb7
Richard Neuboeck <richard@bluedog.at>
2026-07-05 02:07:18 +0200

    added rough clustering of the remaining sequences for train/test/val split and some fixes

--- preprocessing/similarity_search.sh
+++ preprocessing/similarity_search.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -u
 
-# run mmseqs to find similar protein sequences for removal from the dataset to prevent leakage
+# script removes proteins with high similarity in the first stage and then groups
+# the remaining protein sequences to allow splits on cluster boundaries
+# to prevent further leakage
+#
 # mmseqs documentation https://github.com/soedinglab/mmseqs2/wiki
 # 
 # download mmseqs if necessary -> only tested on Fedora/Linux and MacOS!
@@ -24,7 +28,7 @@ MMSEQS="preprocessing/mmseqs/bin/mmseqs"
 pushd "$PROJECT_ROOT"
 
 # checking if binary is available
-if [[ ! -x "$MMSEQS" ]]
+if [[ ! -x $MMSEQS ]]
 then
     echo "mmseqs binary doesn not exist. Trying to download it."
     pushd "$SCRIPT_DIR"
@@ -58,30 +62,69 @@ then
 fi
 
 # create directory(s) if necessary
-if [[ ! -d "$MMSEQS_DIR" ]]
+if [[ -d $MMSEQS_DIR ]]
 then
-    mkdir -p "$MMSEQS_DIR/temp"
+    echo "mmseqs2 directories already exist. Reusing the existing database might lead to unexpected outcomes!"
+else
+    mkdir -p "$MMSEQS_DIR/"{stage1,stage2,temp_stage1,temp_stage2}
 fi
 
-# step 1 combine all proteomes into one faa file
+# stage 1 - remove highly similar sequences
+
+# combine all proteomes into one faa file
 echo "Combining sampled proteomes."
 cat "$PROTEOMES/"*.faa > "$MMSEQS_DIR/proteomes_combined.faa"
 
+echo "Analyzing $(grep '^>' "$MMSEQS_DIR/proteomes_combined.faa" | wc -l) sequences."
+
+echo "Stage 1 : Removing highly similar sequences."
+
 # create mmseqs database
 echo "Creating mmseqs database."    
-"$MMSEQS" createdb "$MMSEQS_DIR/proteomes_combined.faa" "$MMSEQS_DIR/proteomes_combined.db"
+"$MMSEQS" createdb "$MMSEQS_DIR/proteomes_combined.faa" "$MMSEQS_DIR/stage1/proteomes_combined.db"
 
 # cluster the sequences
-echo "Clustering the sequences."
-"$MMSEQS" cluster "$MMSEQS_DIR/proteomes_combined.db" "$MMSEQS_DIR/cluster.db" "$MMSEQS_DIR/temp" --min-seq-id 0.95 -c 0.9 --cov-mode 0
+# --min-seq-id minimum pairwise sequence identity -> high for removing highly similar sequences 0.90-0.99
+# -c minimum coverage of the alignment -> high for highly similar sequences 0.8-0.95
+# --cov-mode require coverage threshold on both sequences (strict) -> 0
+echo "Clustering with high similarity."
+"$MMSEQS" cluster "$MMSEQS_DIR/stage1/proteomes_combined.db" "$MMSEQS_DIR/stage1/cluster.db" "$MMSEQS_DIR/temp_stage1" --min-seq-id 0.95 -c 0.9 --cov-mode 0
 
 # create TSV
 echo "Creating a TSV formatted output."
-"$MMSEQS" createtsv "$MMSEQS_DIR/proteomes_combined.db" "$MMSEQS_DIR/proteomes_combined.db" "$MMSEQS_DIR/cluster.db" "$MMSEQS_DIR/clusters.tsv"
+"$MMSEQS" createtsv "$MMSEQS_DIR/stage1/proteomes_combined.db" "$MMSEQS_DIR/stage1/proteomes_combined.db" "$MMSEQS_DIR/stage1/cluster.db" "$MMSEQS_DIR/stage1/clusters.tsv"
 
 # keeping only one per cluster
-echo "Keeping one sequence per cluster. Output saved to $MMSEQS_DIR/representatives.tsv"
-cut -f1 "$MMSEQS_DIR/clusters.tsv" | sort -u > "$MMSEQS_DIR/representatives.tsv"
+echo "Keeping one sequence per cluster. Output saved to $MMSEQS_DIR/stage1/representatives.txt"
+cut -f1 "$MMSEQS_DIR/stage1/clusters.tsv" | sort -u > "$MMSEQS_DIR/stage1/representatives.txt"
+
+# echo "Exporting representative sequences into new FASTA file."
+"$MMSEQS" createsubdb "$MMSEQS_DIR/stage1/representatives.txt" "$MMSEQS_DIR/stage1/proteomes_combined.db" "$MMSEQS_DIR/stage1/representatives.db" --id-mode 1
+"$MMSEQS" convert2fasta "$MMSEQS_DIR/stage1/representatives.db" "$MMSEQS_DIR/stage1/representatives.faa"
+
+echo "Stage 1 complete: $(wc -l "$MMSEQS_DIR/stage1/representatives.txt") representative sequences remaining."
+
+# stage 2 - clustering the remaining sequences
+
+echo "Stage 2 : Clustering the remaining sequences for train/test/validation split."
+
+# create new database
+echo "Creating new database from the representative sequences."
+"$MMSEQS" createdb "$MMSEQS_DIR/stage1/representatives.faa" "$MMSEQS_DIR/stage2/representatives.db"
+
+# cluster
+# --min-seq-id minimum pairwise sequence identity -> low to group distant relatives 0.3-0.6
+# -c minimum coverage of the alignment -> lower coverage 0.5-0.8
+# --cov-mode require coverage threshold on both sequences (strict) -> 0
+# --cluster-mode -> 1 to connect clusters transitively
+echo "Clustering with low similarity."
+"$MMSEQS" cluster "$MMSEQS_DIR/stage2/representatives.db" "$MMSEQS_DIR/stage2/cluster.db" "$MMSEQS_DIR/temp_stage2" --min-seq-id 0.3 -c 0.9 --cov-mode 0
+
+echo "Creating a TSV formatted output."
+"$MMSEQS" createtsv "$MMSEQS_DIR/stage2/representatives.db" "$MMSEQS_DIR/stage2/representatives.db" "$MMSEQS_DIR/stage2/cluster.db" "$MMSEQS_DIR/stage2/split_clusters.tsv"
+
+CLUSTERS=$(cut -f1 "$MMSEQS_DIR/stage2/split_clusters.tsv" | sort -u | wc -l)
+echo "Stage 2 complete. $CLUSTERS identified. Output written to $MMSEQS_DIR/stage2/split_clusters.tsv."
 
 # return to where we where
 popd
